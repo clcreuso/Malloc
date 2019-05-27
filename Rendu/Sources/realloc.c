e498b7a00b21a7c3690de950d54849cfadc875b9
Clément Creusot <clement@MacBook-Pro-de-Clement.local>
2019-05-27 17:53:44 +0200

    add: - adding function for realloc a memory zone

--- Rendu/Sources/realloc.c
+++ Rendu/Sources/realloc.c
@@ -6,14 +6,29 @@
 /*   By: clement <clement@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/05/08 14:10:16 by clement      #+#   ##    ##    #+#       */
-/*   Updated: 2019/05/08 15:21:19 by clement     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/05/27 16:54:41 by clement     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include "../Includes/realloc.h"
+#include "../Includes/malloc.h"
 
-void	*realloc(void *ptr, size_t size)
+void	*ft_realloc(void *ptr, size_t size)
 {
-	return (NULL);
+	t_region	*region;
+	t_chunk		*chunk;
+	void		*new_ptr;
+
+	chunk = (t_chunk *)(ptr - sizeof(struct s_chunk));
+	region = (t_region *)chunk->region;
+	
+	if (chunk->size >= (size + sizeof(struct s_chunk)))
+		return (ptr);
+		
+	if ((new_ptr = ft_malloc(size)))
+		ft_strncpy(new_ptr, ptr, (chunk->size - sizeof(struct s_chunk)));
+	
+	ft_free(ptr);
+		
+	return (new_ptr);
 }
