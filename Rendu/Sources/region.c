c5280e3bbcdce3636d9a2c8d5d1c6fe896ad953c
Clément Creusot <clement@MacBook-Pro-de-Clement.local>
2019-05-27 18:00:23 +0200

    add: - adding functions for managment of memory region

---
+++ Rendu/Sources/region.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   region.c                                         .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: clement <clement@student.le-101.fr>        +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/05/24 15:07:22 by clement      #+#   ##    ##    #+#       */
+/*   Updated: 2019/05/27 14:38:27 by clement     ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "../Includes/malloc.h"
+
+t_region	*find_metadata_region(t_region *region, int type)
+{
+	t_region	*ptr;
+
+	ptr = (region) ? region->next : g_region;
+	while (ptr)
+	{
+		if (ptr->type == type && ptr->used != ptr->size)
+			return ptr;
+		ptr = ptr->next;
+	}
+	
+	ptr = g_region;
+
+	while (ptr->next)
+	{
+		if (!(ptr->type))
+			return ptr;
+		ptr = ptr->next;
+	}
+	
+	return (NULL);
+}
+
+t_region	*new_metadata_region()
+{
+	t_region	*ptr;
+	t_region	*result;
+	void		*threshold;
+
+	ptr = (t_region *)mmap_page(REGION_MMAP_SIZE);
+	threshold = mmap_page(ZERO);
+	result = ptr;
+	result->start = 1;
+	result->prev = NULL;
+	while ((void *)ptr + (sizeof(struct s_region) * 2) < threshold)
+	{
+		ptr->next = (void *)ptr + sizeof(struct s_region);
+		ptr = ptr->next;
+		ptr->prev = (void *)ptr - sizeof(struct s_region);
+	}
+	ptr->next = NULL;
+	
+	return (result);
+}
+
+void		init_metadata_region(void)
+{
+	t_region	*region;
+	
+	if (!g_region)
+		g_region = new_metadata_region();
+	else
+	{
+		region = g_region;
+		
+		while (region->next)
+		{
+			if (!(region->type))
+				return ;
+			region = region->next;
+		}
+		region->next = new_metadata_region();
+	}
+}
