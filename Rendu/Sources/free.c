e20fadc0d1b455ce20306d6a3c6e66b8e4b9854a
Clément Creusot <clement@MacBook-Pro-de-Clement.local>
2019-05-27 17:51:37 +0200

    add: - adding functions to free mermory

--- Rendu/Sources/free.c
+++ Rendu/Sources/free.c
@@ -6,14 +6,67 @@
 /*   By: clement <clement@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/05/08 14:49:31 by clement      #+#   ##    ##    #+#       */
-/*   Updated: 2019/05/08 15:02:26 by clement     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/05/27 16:54:14 by clement     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "../Includes/free.h"
-	
-void	free(void *ptr)
+#include "../Includes/malloc.h"
+
+void	free_region(t_region *region)
 {
+	if (!(region->used) && (region->type))
+	{
+		munmap(region->heap, region->size);
+		region->type = 0;
+		region->size = 0;
+		region->used = 0;
+		region->heap = NULL;
+	}
+}
+
+void	free_metadata()
+{
+	t_region *region;
+	t_region *tmp;
+
+	region = g_region;
+	while (region->next)
+	{
+		if (region->type)
+			return ;
+		region = region->next;
+	}
+	if (region->type)
+		return ;
+	while (region)
+	{
+		tmp = region->prev;
+		if (region->start)
+			munmap(region, 4096);
+		region = tmp;
+	}
+	g_region = NULL;
+}
+
+void	ft_free(void *ptr)
+{
+	t_region	*region;
+	t_chunk		*chunk;
 	
+	if (!(ptr))
+		return ;
+
+	chunk = (t_chunk *)(ptr - sizeof(struct s_chunk));
+	region = (t_region *)chunk->region;
+		
+	if (!(IS_FREE(chunk->size)))
+	{
+		chunk->size -= 1;
+		region->used -= chunk->size;
+	}
+	
+	free_region(region);
+	free_metadata();
 }
