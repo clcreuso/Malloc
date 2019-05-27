9bc89bac5794be33b686c3a926fed74d44b56170
Clément Creusot <clement@MacBook-Pro-de-Clement.local>
2019-05-27 18:02:56 +0200

    add: - adding files for test the project

--- Rendu/Sources/debug.c
+++ Rendu/Sources/debug.c
@@ -1,30 +1,31 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   main.c                                           .::    .:/ .      .::   */
+/*   debug.c                                          .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: clement <clement@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2019/05/07 17:47:26 by clement      #+#   ##    ##    #+#       */
-/*   Updated: 2019/05/27 17:58:23 by clement     ###    #+. /#+    ###.fr     */
+/*   Created: 2019/05/24 15:22:01 by clement      #+#   ##    ##    #+#       */
+/*   Updated: 2019/05/27 18:01:32 by clement     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/resource.h>
-#include "Includes/malloc.h"
+#include "../Includes/malloc.h"
 
-void	*ft_malloc(size_t size);
-
-int main()
+void		ft_print_region()
 {
-	char *str;
-	
-	while (1)
-		str = (char*)ft_malloc(100000);
+	t_region	*region_ptr;
+	t_chunk		*chunk_ptr;
+	int			reg = 0;
 
-	return (0);
+	region_ptr = g_region;
+	while (region_ptr)
+	{
+		if (region_ptr->used)
+			printf("START = %d\tREG\t%d\tUSED\t%d / %d\t%d\tADD\t%pHEAP\t%p\n", region_ptr->start, reg, region_ptr->used, region_ptr->size, region_ptr->type, region_ptr, region_ptr->heap);
+		reg++;
+		region_ptr = region_ptr->next;
+	}
+	system("clear");
 }
